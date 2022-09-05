735406978718192e44c282c17e9ab1c5e209843f
mukarram1999 <mukarramali1999@gmail.com>
2022-09-05 11:06:57 -0500

    Tried working with functions for pcost.py.

--- Solutions/1_27/pcost.py
+++ Solutions/1_27/pcost.py
@@ -1,13 +1,16 @@
 # pcost.py
+# Modified by Mukarram Faridi to work as a function
 
-total_cost = 0.0
+def portfolio_cost(filename): 
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            row = line.split(',')
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    print('Total cost:', total_cost)
 
-with open('../../Work/Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        total_cost += nshares * price
-
-print('Total cost', total_cost)
+cost = portfolio_cost('Work/Data/portfolio.csv')
+print("Total cost:", cost)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0.0;
-with open("Data/portfolio.csv", "rt") as f:
-    header = next(f)
-    for line in f:
-        row = line.split(",")
-        cost = int(row[1]) * float(row[2])
-        total_cost = total_cost + cost
-print("Total cost is ", total_cost)
+def portfolio_cost(filename):
+    total_cost = 0.0;
+    with open(filename, "rt") as f:
+        header = next(f)
+        for line in f:
+            row = line.split(",")
+            cost = int(row[1]) * float(row[2])
+            total_cost = total_cost + cost
+    print("Total cost:", total_cost)
+
+cost = portfolio_cost("Work/Data/portfolio.csv")
+print("Total cost:", cost)
